18706a22dce0bba70c916e6266e318868f455cc9
Pierre <pld@pld.local>
2017-04-06 15:56:03 +0200

    Create the npm package anonymize-personal-data

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var Shield = require('./privacyShield.js');
+var Shield = require('./anonymize.js');
 
 var anon = Shield.removePersonalData("Je vais visiter Massongy ce weekend, puis j'irais a paris, et mon 06 est 0670464711 ou 0033 4 50 36 54 88 ou 0540145458", "fr");
 
