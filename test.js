c394f409c7dacedb3b3df96e314cf1c5c0148f37
Pierre <pld@pld.local>
2017-04-06 16:12:14 +0200

    better test

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-var Shield = require('./anonymize.js');
+var Anonymize = require('./anonymize.js');
 
-var anon = Shield.removePersonalData("Je vais visiter Massongy ce weekend, puis j'irais a paris, et mon 06 est 0670464711 ou 0033 4 50 36 54 88 ou 0540145458", "fr");
+var anoymizedData = 
+  Anonymize.removePersonalData("Je vais visiter Massongy ce weekend, puis j'irais a paris, et mon 06 est 0670464711 ou 0033 4 50 36 54 88 ou 0540145458", "fr");
 
-var restored = Shield.restorePersonalData(anon.text, anon.data);
+var unAnoymizedData = Anonymize.restorePersonalData(anoymizedData.text, anoymizedData.data);
 
-console.log(anon, restored);
+console.log(annoymizedData, restored);
